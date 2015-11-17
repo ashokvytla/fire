41d0bf29ea9aca24a23fb5b407ee278881413adf
cloudera <cloudera@clouderahost.(none)>
2015-11-17 11:39:11 -0800

    Add HBase Node and HBase workflow to read data from CSV file and put into HBase Table

---
+++ core/src/main/java/fire/nodes/hbase/HBaseNode.java
@@ -0,0 +1,141 @@
+package fire.nodes.hbase;
+
+import fire.dataframeutil.DataFrameUtil;
+import fire.workflowengine.Node;
+import fire.workflowengine.WorkflowContext;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+import scala.Function1;
+import scala.collection.Seq;
+import scala.collection.mutable.ArrayBuffer;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.*;
+
+
+/**
+ * Created by Ashok Rajan on 11/16/15.
+ */
+public class HBaseNode extends Node implements Serializable {
+
+    public HBaseNode(int i, String nm) {
+        super(i, nm);
+    }
+
+    @Override
+    public void execute(JavaSparkContext ctx, SQLContext sqlContext, WorkflowContext workflowContext, DataFrame df) {
+
+        workflowContext.out("Executing HBaseNode : " + id);
+
+        //testMapping(sqlContext);
+       // mapColumns(sqlContext);
+
+        writeToHbase(batchPuts(df));
+
+        super.execute(ctx, sqlContext, workflowContext, df);
+    }
+
+    private void testMapping(SQLContext sqlContext){
+        String mapperConfigFile = "/home/cloudera/fireprojects/fire/data/hbasemapper.json";
+        DataFrame people = sqlContext.jsonFile(mapperConfigFile);
+        people.registerTempTable("people");
+
+        DataFrame schools = people.select("schools");
+        schools.registerTempTable("schools");
+        System.out.println("========= schools.printSchema()===========");
+        schools.printSchema();
+        System.out.println("========= schools.show();============");
+        schools.show();
+        System.out.println("=====================");
+
+        Row[] rows = schools.collect();
+        Row row = rows[0];
+
+
+        DataFrame school = schools.select("sname","year");
+        school.registerTempTable("school");
+        System.out.println("========= school.printSchema()===========");
+
+        school.printSchema();
+        System.out.println("========= school.printSchema()===========");
+        school.show();
+        System.out.println("===================");
+
+
+    }
+
+    private void mapColumns(SQLContext sqlContext) {
+        String mapperConfigFile = "/home/cloudera/fireprojects/fire/data/hbasemapper.json";
+
+        DataFrame mapperConfig = sqlContext.jsonFile(mapperConfigFile);
+        mapperConfig.registerTempTable("mapperConfig");
+
+        System.out.println("=========mapperConfig.printSchema() ============");
+        mapperConfig.printSchema();
+        System.out.println("==========================================");
+
+        DataFrame columnfm = sqlContext.sql("SELECT columnfamily.column FROM mapperConfig");
+        Iterator<Row> columnsIterator  = columnfm.collectAsList().iterator();
+        while(columnsIterator.hasNext()){
+            Row row = columnsIterator.next();
+            System.out.println(row.get(0));
+        }
+
+//        List<Row> mappers = mapperConfig.collectAsList();
+//        Iterator<Row> rowIterator = mappers.iterator();
+//        while (rowIterator.hasNext()) {
+//            Row mapping = rowIterator.next();
+//            System.out.println(mapping.toString());
+//        }
+
+    }
+
+    private List<Put> batchPuts(DataFrame df) {
+        List<Put> putList = new ArrayList<Put>();
+        Put p = null;
+        Iterator<Row> rows = df.collectAsList().iterator();
+        while (rows.hasNext()) {
+            Row row = rows.next();
+            System.out.println(row.get(0) + "," + row.get(1) + "," + row.get(2) + "," + row.get(3));
+
+            p = new Put(Bytes.toBytes(row.getString(0)));
+            p.addColumn(Bytes.toBytes("persondetails"), Bytes.toBytes("fullname"), Bytes.toBytes(row.getString(1)));
+            p.addColumn(Bytes.toBytes("persondetails"), Bytes.toBytes("age"), Bytes.toBytes(row.getString(2)));
+            p.addColumn(Bytes.toBytes("persondetails"), Bytes.toBytes("city"), Bytes.toBytes(row.getString(3)));
+            putList.add(p);
+        }
+        return putList;
+    }
+
+
+
+    private void writeToHbase(List<Put> putList){
+        Connection conn = null;
+        try{
+            Configuration configuration = HBaseConfiguration.create();
+            configuration.addResource(new Path("/etc/hbase/conf.cloudera.hbase/hbase-site.xml"));
+            conn =   ConnectionFactory.createConnection(configuration);
+            Table t1 = conn.getTable(TableName.valueOf("person"));
+            t1.put(putList);
+            t1.close();
+            Admin admin = conn.getAdmin();
+            conn.close();
+        }catch (IOException e){
+            System.out.println("Error writing to HBase "+e.getMessage());
+        }
+    }
+
+}
---
+++ examples/src/main/java/fire/examples/hbase/WorkflowHBaseETL.java
@@ -0,0 +1,74 @@
+package fire.examples.hbase;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fire.nodes.dataset.NodeDatasetFileOrDirectoryCSV;
+import fire.nodes.hbase.HBaseNode;
+import fire.nodes.ml.NodeKMeans;
+import fire.sparkutil.CreateSparkContext;
+import fire.workflowengine.Workflow;
+import fire.workflowengine.WorkflowContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+
+/**
+ * Created by Ashok Rajan
+ */
+public class WorkflowHBaseETL {
+
+    //--------------------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        // create spark and sql context
+        JavaSparkContext ctx = CreateSparkContext.create(args);
+
+        SQLContext sqlContext = new SQLContext(ctx);
+
+        WorkflowContext workflowContext = new WorkflowContext();
+
+        hbase(ctx, sqlContext, workflowContext);
+
+        // stop the context
+        ctx.stop();
+    }
+
+
+    //--------------------------------------------------------------------------------------
+
+    // HBase workflow
+    private static void hbase(JavaSparkContext ctx, SQLContext sqlContext, WorkflowContext workflowContext) {
+
+        Workflow wf = new Workflow();
+
+        // CSV node
+        NodeDatasetFileOrDirectoryCSV csv1 = new NodeDatasetFileOrDirectoryCSV(1, "csv1 node", "/home/cloudera/fireprojects/fire/data/person.csv",
+                "personid fn age city", "string string string string",
+                "numeric numeric numeric numeric");
+        wf.addNodeDataset(csv1);
+
+        // HBase node
+        HBaseNode hBaseNode = new HBaseNode(10, "hbase node");
+        csv1.addNode(hBaseNode);
+
+        // execute the workflow
+        wf.execute(ctx, sqlContext, workflowContext);
+
+    }
+
+}

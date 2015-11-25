5fd8f7f11fd60a67dbcda9ebc1c3e7c2e36ade7a
Jayant Shekhar <jayantshekhar@Jayants-iMac.local>
2015-11-25 00:23:33 -0800

    node to load data into solr

--- core/src/main/java/fire/nodes/solr/NodeSolrLoad.java
+++ core/src/main/java/fire/nodes/solr/NodeSolrLoad.java
@@ -1,13 +1,19 @@
 package fire.nodes.solr;
 
+import com.lucidworks.spark.SolrSupport;
 import fire.util.spark.DataFrameUtil;
 import fire.workflowengine.Node;
 import fire.workflowengine.WorkflowContext;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+import scala.Tuple2;
 
 /**
  * Created by nikhil
@@ -16,11 +22,23 @@ public class NodeSolrLoad extends Node {
 
     public String collection = "test";
 
+    // dataframe id column
+    public String idcol = "id";
+
     // dataframe columns
     public String dfcols = "c1 c2 c3";
 
     // solr columns
     public String solrcols = "s1 s2 s3";
+
+    String zkHost;
+
+
+    int queueSize = 1000;
+
+    int numRunners = 2;
+
+    int pollQueueTime = 20;
 
     public NodeSolrLoad() {}
 
@@ -45,32 +63,40 @@ public class NodeSolrLoad extends Node {
         // find the indexes of the data frame columns
         final int[] dcolsidx = DataFrameUtil.getColumnIndexes(df, dfcols);
 
+        // solr columns
+        final String[] solrColsarr = this.solrcols.split(" ");
+
         // load the records into solr
-        df.toJavaRDD().map(new LoadRecordIntoSolr(dcolsidx));
-    }
+        //df.toJavaRDD().map(new LoadRecordIntoSolr(dcolsidx));
 
-}
+        JavaPairRDD<String,SolrInputDocument> pairs = df.toJavaRDD().mapToPair(new PairFunction<Row, String, SolrInputDocument>() {
 
-class LoadRecordIntoSolr implements Function<Row, Row> {
+            public Tuple2<String, SolrInputDocument> call(Row row) throws Exception {
+                SolrInputDocument doc = new SolrInputDocument();
 
-    int[] dcolsidx;
+                // get the values to be inserted from the row index
+                for (int i = 0; i<dcolsidx.length; i++) {
+                    doc.setField(solrColsarr[i], row.getString(dcolsidx[i]));
+                }
 
-    LoadRecordIntoSolr(int[] colsidx) {
-        dcolsidx = colsidx;
-    }
+                return new Tuple2<String, SolrInputDocument>((String) doc.getFieldValue(idcol), doc);
+            }
+        });
 
-    public Row call(Row r) {
+        SolrSupport.indexDocs(zkHost, collection, 100, pairs.values());
 
-        // array of values
-        String[] validx = new String[dcolsidx.length];
+        // send a final commit in case soft auto-commits are not enabled
+        CloudSolrClient cloudSolrClient = SolrSupport.getSolrServer(zkHost);
+        cloudSolrClient.setDefaultCollection(collection);
 
-        // get the values to be inserted from the row index
-        for (int i = 0; i<dcolsidx.length; i++) {
-            validx[i] = r.getString(dcolsidx[i]);
+        try {
+            cloudSolrClient.commit(true, true);
+            cloudSolrClient.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
         }
 
-        // insert the record into solr
-
-        return null;
     }
+
 }
+

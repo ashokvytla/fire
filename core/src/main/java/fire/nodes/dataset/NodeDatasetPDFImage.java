e1b1ba18db68c799de9270c8b8a45765c9669379
Jayant Shekar <jayant@user-MBPMBA-3.local>
2015-11-25 19:46:28 -0800

    added node for processing pdf images

---
+++ core/src/main/java/fire/nodes/dataset/NodeDatasetPDFImage.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fire.nodes.dataset;
+
+import fire.util.fileformats.pdf.PdfInputFormat;
+import fire.util.spark.SchemaUtil;
+import fire.workflowengine.WorkflowContext;
+import org.apache.hadoop.io.Text;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.input.PortableDataStream;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.ghost4j.document.PDFDocument;
+import org.ghost4j.renderer.SimpleRenderer;
+import scala.Tuple2;
+
+import java.awt.*;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by jayantshekhar
+ * Represents a Dataset Node which points to data in a File or Directory with data in csv format.
+ */
+
+public class NodeDatasetPDFImage extends NodeDatasetFileOrDirectory implements Serializable {
+
+    // key column
+    public String kcol = "key";
+
+    // value column
+    public String vcol = "val";
+
+    public NodeDatasetPDFImage(int i, String nm, String p) {
+        super(i, nm, p);
+    }
+
+    public NodeDatasetPDFImage(int i, String nm, String p, String cols, String colTypes, String colmlTypes) {
+        super(i, nm, p, cols, colTypes);
+    }
+
+    public NodeDatasetPDFImage()
+    {
+
+    }
+
+    //------------------------------------------------------------------------------------------------------
+
+    @Override
+    public void execute(JavaSparkContext ctx, SQLContext sqlContext, WorkflowContext workflowContext, DataFrame df) {
+
+        workflowContext.out("Executing NodeDatasetPDFImage : " + id);
+
+        JavaPairRDD<String, PortableDataStream> files = ctx.binaryFiles (path);
+
+        JavaRDD<Row> imagesrdd =  files.map(new ConvertFunction());
+
+        long cnt = imagesrdd.count();
+
+        super.execute(ctx, sqlContext, workflowContext, null);
+    }
+
+    //------------------------------------------------------------------------------------------------------
+
+}
+
+class ConvertFunction implements Function<Tuple2<String, PortableDataStream>, Row> {
+
+    public Row call(Tuple2<String, PortableDataStream> f) throws Exception {
+
+        PDFDocument document = new PDFDocument( );
+        document.load(f._2().open());
+        f._2().close();
+
+        SimpleRenderer renderer = new SimpleRenderer();
+        renderer.setResolution(300);
+        List<Image> images = renderer.render(document);
+
+        return RowFactory.create(f._1(), images);
+    }
+}
+
